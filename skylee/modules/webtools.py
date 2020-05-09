1b1f2030470f7ed1dcc8cf049909ab89b6509780
renvenous <59575056+renvenous@users.noreply.github.com>
2020-05-09 21:06:03 +0530

    Add files via upload

--- skylee/modules/webtools.py
+++ skylee/modules/webtools.py
@@ -23,11 +23,11 @@ from skylee.modules.helper_funcs.alternate import send_message
 @typing_action
 def ping(update, context):
 	start_time = time.time()
-	test = send_message(update.effective_message, "Pong!")
-	end_time = time.time()
-	ping_time = float(end_time - start_time)
-	context.bot.editMessageText(chat_id=update.effective_chat.id, message_id=test.message_id,
-						text=tl(update.effective_message, "Pong!\nKecepatannya: {0:.2f} detik").format(round(ping_time, 2) % 60))
+	test = send_message(update.effective_message, "PONG!!\n"
+                 "<b>Time Taken:</b> <code>{}</code>\n"
+                 "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime))
+
+    update.effective_message.reply_text(reply_msg, parse_mode=ParseMode.HTML)
 
 #Kanged from PaperPlane Extended userbot
 def speed_convert(size):
