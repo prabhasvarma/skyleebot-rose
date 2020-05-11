3be38151d9d82635b6865b8ca5f676a619b47fda
renvenous <59575056+renvenous@users.noreply.github.com>
2020-05-11 21:56:13 +0530

    Add files via upload

--- skylee/__main__.py
+++ skylee/__main__.py
@@ -147,7 +147,7 @@ def start(update, context):
         else:
             update.effective_message.reply_text(PM_START_TEXT, reply_markup=InlineKeyboardMarkup(buttons), parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
     else:
-        update.effective_message.reply_text("Excuse me, is your name {}? Because you look like a hot-tea!").format(update.effective_user.first_name))
+        update.effective_message.reply_text("Excuse me, is your name {}? Because you look like a hot-tea!".format(mention_html(user_chat.id, user_chat.first_name)),
 
 
 # for test purposes
